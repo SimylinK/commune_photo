11e4d88db6b3dbcdcc226ed719f396c0aa20ec4a
SimylinK <maraval.nathan@gmail.com>
2016-03-25 15:10:19 +0100

    requete ajax marche

--- script/script.js
+++ script/script.js
@@ -10,23 +10,44 @@ $(function(){
   });
 
 
-  $("#button").on('click', function(){
+/*  $("#button").on('click', function(){
     $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?", {
-        tags: $("#commune").val(),
-        tagmode: "any",
-        format: "json"
+      tags: $("#commune").val(),
+      tagmode: "any",
+      format: "json"
     }, function(data) {
         $("#dvImages").html("");
         $.each(data.items, function(i, item) {
             var img = $("<img/>");
             img.attr('width', '400px');
             img.attr('height', '250px');
-            img.attr("src", item.media.m).appendTo("#dvImages");
+            img.attr("src", "http://www.flickr.com/photos/" + item.photos.photo.owner + "/" + item.photos.photo.id +"/").appendTo("#dvImages");
             if (i == $("#listNumber").val()-1) return false;
         });
     });
   });
 
 
+
+});*/
+
+
+$("#button").on('click', function(){
+  $.getJSON("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=5149a64fa91469647a7511af9adf33a5&format=json&nojsoncallback=1", {
+      sort:"relevance",
+      tags: $("#commune").val()
+
+  }, function(data) {
+
+      $("#dvImages").html("");
+      $.each(data.photos.photo, function(i, item) {
+          var img = $("<img/>");
+          img.attr('width', '400px');
+          img.attr('height', '250px');
+          img.attr("src", "https://farm"+ item.farm +".staticflickr.com/"+ item.server +"/"+ item.id +"_"+ item.secret +".jpg").appendTo("#dvImages");
+          if (i == $("#listNumber").val()-1) return false;
+      });
+  });
+});
 
 });
