8b1420cc4bee19cee24da650fe7ba67bcd8ddbd0
SimylinK <maraval.nathan@gmail.com>
2016-03-25 18:18:51 +0100

    vue photo bien entamée

--- script/script.js
+++ script/script.js
@@ -1,3 +1,15 @@
+function displayInfo(photoID, divID) {
+  $.getJSON("https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=5149a64fa91469647a7511af9adf33a5&format=json&nojsoncallback=1", {
+      photo_id:photoID
+  }, function(data) {
+      $("#" + divID).html("Titre : " + data.photo.title._content + "</br>" +
+        "Date : " + data.photo.dates.taken + "</br>" +
+        "Photographe : " + data.photo.owner.realname)
+      $("#" + divID).dialog("open");
+  });
+}
+
+
 $(function(){
 
   var sel = $('#listNumber');
@@ -87,15 +99,27 @@ $(function(){
       $.each(data.photos.photo, function(i, item) {
           vide = false;
           var img = $("<img/>");
-          img.attr('width', '400px');
-          img.attr('height', '250px');
-          img.attr("src", "https://farm"+ item.farm +".staticflickr.com/"+ item.server +"/"+ item.id +"_"+ item.secret +".jpg").appendTo("#dvImages");
 
 
-          var imgTxt = "<img src=\"https://farm"+ item.farm +".staticflickr.com/"+ item.server +"/"+ item.id +"_"+ item.secret +".jpg\" height=\"250px\" width=\"400px\" onclick=\"alert(\'TEST\')\">"
+          var imgPhoto = "<img src=\"https://farm"+ item.farm +".staticflickr.com/"+ item.server +"/"+ item.id +"_"+ item.secret +".jpg\" height=\"250px\" width=\"400px\" onclick=\"displayInfo(\'"+ item.id +"\', \'div"+ i.toString() +"\')\">"
+          $("#dvImages").append(imgPhoto);
+
+          var div ="<div id=\"div"+i.toString()+"\"> Cédric est un PD</div>";
+          $("#dvImages").append(div);
+          $("#div" + i.toString()).dialog({
+            autoOpen: false,
+            height: 280,
+            width: 400,
+            modal: true,
+            dialogClass: 'dlgfixed',
+            position: "center",
+          })
+          $("#dvImages").append("</br>");
+
+          var imgTable = "<img src=\"https://farm"+ item.farm +".staticflickr.com/"+ item.server +"/"+ item.id +"_"+ item.secret +".jpg\" height=\"250px\" width=\"400px\">"
           // $("#affichage").append("<tr><td id='test'>test</td></tr>");
 
-          getInfo(item.id, imgTxt);
+          getInfo(item.id, imgTable);
 
           if (i == $("#listNumber").val()-1) return false;
       });
@@ -105,16 +129,7 @@ $(function(){
 
 
 
-  function displayInfo(photoID) {
-    $.getJSON("https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=5149a64fa91469647a7511af9adf33a5&format=json&nojsoncallback=1", {
-        photo_id:photoID
-    }, function(data) {
-        $("#dialog-infoPhoto").html("Titre : " + data.photo.title._content + "</br>" +
-          "Date : " + data.photo.dates.taken + "</br>" +
-          "Photographe : " + data.photo.owner.realname)
-        $("#dialog-infoPhoto").dialog("open");
-    });
-  }
+
 
 
   function getInfo(photoID, imgTxt) {
